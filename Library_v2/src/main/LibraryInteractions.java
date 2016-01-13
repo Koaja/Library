2392d18294705d3ccd05611bb915e9e7430a5909
BaleaCristi <balea.cristian90@gmail.com>
2016-01-13 12:22:08 +0200

    added Search and Sort to LibraryV2

--- Library_v2/src/main/LibraryInteractions.java
+++ Library_v2/src/main/LibraryInteractions.java
@@ -3,11 +3,12 @@ package main;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class LibraryInteractions {
 
@@ -15,7 +16,7 @@ public class LibraryInteractions {
 
 	BufferedReader readBooksFromFile;
 	PrintStream writeBooksToFile;
-	File booksFile = new File("C:\\Users\\Koaja\\Desktop\\books.txt");
+	File booksFile = new File("C:\\Users\\Balea Cristian\\Downloads\\books.txt");
 
 	public void addBook(Book b) {
 		books.add(b);
@@ -25,6 +26,35 @@ public class LibraryInteractions {
 		for (Book book : books) {
 			System.out.println(book);
 		}
+	}
+
+	public void searchBook(String text) {
+		boolean searchSuccessful = false;
+		for (Book book : books) {
+			if (book.toString().contains(text)) {
+				System.out.println(book);
+				searchSuccessful = true;
+			}
+		}
+
+		if (searchSuccessful == false) {
+			System.out.println("No matches were found");
+		}
+	}
+
+	public int getNumberOfBooks() {
+		return books.size();
+	}
+
+	public void sortBooks() {
+		Collections.sort(books, new Comparator<Book>() {
+
+			@Override
+			public int compare(Book o1, Book o2) {
+				return o1.toString().compareToIgnoreCase(o2.toString());
+			}
+
+		});
 	}
 
 	public void importBooks() {
@@ -57,25 +87,16 @@ public class LibraryInteractions {
 
 	public void exportBooks() {
 
+		booksFile.delete();
 		try {
+			booksFile.createNewFile();
 			writeBooksToFile = new PrintStream(booksFile);
-			readBooksFromFile = new BufferedReader(new InputStreamReader(new FileInputStream(booksFile)));
-		} catch (FileNotFoundException e) {
+			for (Book book : books) {
+				writeBooksToFile.println(book);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-		for (Book b : books) {
-			String a = b.toString();
-			try {
-				if(!a.equals(readBooksFromFile.readLine())){
-					writeBooksToFile.println(a);
-				}else{
-					System.out.println("file was already there");
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
 	}
 }
--- Library_v2/src/main/Main.java
+++ Library_v2/src/main/Main.java
@@ -1,17 +1,84 @@
 package main;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		LibraryInteractions lib = new LibraryInteractions();
+	public static void main(String[] args) {
 
-		lib.importBooks();
-		Book b = new Book("dsadsa", "dsadsa", "dsadas"); 
-		lib.addBook(b);
-		lib.showBooks();
-		System.out.println();
-		System.out.println();
-		lib.exportBooks();
+		LibraryInteractions lib = new LibraryInteractions();
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		Book book;
+
+		String[] line = null;
+		String search;
+		String userMenuChoice = "";
+		while (true) {
+			System.out.println("1. Add book");
+			System.out.println("2. Search");
+			System.out.println("3. List");
+			System.out.println("4. Export libray");
+			System.out.println("5. Import library");
+			System.out.println("6. Quit");
+
+			System.out.print("Choose your action: ");
+			try {
+				userMenuChoice = bf.readLine();
+				if (userMenuChoice.equals("1")) {
+					System.out.print("Enter Book Author: ");
+					String bookAuthor = bf.readLine();
+
+					System.out.print("Enter Book Title: ");
+					String bookTitle = bf.readLine();
+
+					System.out.print("Enter Book Genre: ");
+					String bookGenre = bf.readLine();
+
+					if (!bookAuthor.isEmpty() && !bookTitle.isEmpty()) {
+						book = new Book(bookAuthor, bookTitle, bookGenre);
+						lib.addBook(book);
+						System.out.println(
+								"Book " + bookAuthor + " - " + bookTitle + " was successfuly added in your library.\n");
+					} else {
+						System.out.println("Author or Title cannot be empty");
+					}
+
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (userMenuChoice.equals("2")) {
+				System.out.print("Type author or book title: ");
+				try {
+					search = bf.readLine();
+					lib.searchBook(search);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+			}
+
+			if (userMenuChoice.equals("3")) {
+				lib.showBooks();
+
+				System.out.println();
+				System.out.println("You currently have " + lib.getNumberOfBooks() + " books in your library:");
+			}
+
+			if (userMenuChoice.equals("4")) {
+				lib.exportBooks();
+			}
+
+			if (userMenuChoice.equals("5")) {
+				lib.importBooks();
+			}
+
+			if (userMenuChoice.equals("6")) {
+				lib.sortBooks();
+			}
+		}
+
 	}
 }
